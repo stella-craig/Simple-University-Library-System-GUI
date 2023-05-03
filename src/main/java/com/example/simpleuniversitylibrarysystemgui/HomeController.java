9601ee5f05f0e6760757f46f5998cd1c16bdf33a
stella-craig <stellarstellacraig@gmail.com>
2023-05-03 15:12:07 -0500

    commit -Stella

--- src/main/java/com/example/simpleuniversitylibrarysystemgui/HomeController.java
+++ src/main/java/com/example/simpleuniversitylibrarysystemgui/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.simpleuniversitylibrarysystemgui;
 
+import com.sun.glass.ui.ClipboardAssistance;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -29,5 +30,78 @@ public class HomeController {
         stage.setTitle("New-Collection");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    protected void onRemoveMembershipButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("removeMember.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Remove-Membership");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onRemoveItemButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("removeItem.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Remove-Item");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onNewEmployeeButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("newEmployee.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("New-Employee");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onBorrowItemButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("checkoutItem.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Borrow-Item");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onReturnItemButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("returnItem.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Return-Item");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onCheckOverduesButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("checkOverdue.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Check-Overdue");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onEditInformationButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("editInformation.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Edit-Information");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    protected void onQuitButtonClick() throws IOException {
     }
 }
