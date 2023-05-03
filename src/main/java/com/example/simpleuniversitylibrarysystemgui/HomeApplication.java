47c64044c3be1a57162bf0ee3616d2c481d8eb61
stella-craig <stellarstellacraig@gmail.com>
2023-05-03 14:14:55 -0500

    commit message

---
+++ src/main/java/com/example/simpleuniversitylibrarysystemgui/HomeApplication.java
@@ -0,0 +1,24 @@
+package com.example.simpleuniversitylibrarysystemgui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class HomeApplication extends Application
+{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
