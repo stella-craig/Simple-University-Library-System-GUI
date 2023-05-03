c2042fee68a7203a346864e0ed947a3685f72bba
stella-craig <stellarstellacraig@gmail.com>
2023-05-03 14:33:00 -0500

    commit message

--- src/main/java/com/example/simpleuniversitylibrarysystemgui/HomeController.java
+++ src/main/java/com/example/simpleuniversitylibrarysystemgui/HomeController.java
@@ -1,13 +1,25 @@
 package com.example.simpleuniversitylibrarysystemgui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public class HomeController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onNewMembershipButtonClick() {
-        welcomeText.setText("This is very stupid!!!!");
+    protected void onNewMembershipButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("newMember.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("New-Member");
+        stage.setScene(scene);
+        stage.show();
     }
 }
